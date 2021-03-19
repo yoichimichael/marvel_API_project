e25be1edbd4a98b4f606a152ef2966c0aa8b2381
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-03-19 01:01:15 -0400

    change env file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
-const square = document.querySelector('.container');
+
+
+
+// const square = document.querySelector('.container');
 console.log(process.env);
 // console.log(process.env.PRIVATE_KEY);
 // console.log("hello")
