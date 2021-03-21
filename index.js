23a237c9ec45daed9a70d2fa6081473aa8cc0bcf
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-03-20 21:49:03 -0400

    more api testing

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ts = Date.now();
 const hash = md5(`${ts}${PRIVATE_KEY}${PUBLIC_KEY}`)
-const URL = `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
+const URL = `http://gateway.marvel.com/v1/public/characters?name=Spider-Man&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
 
 // let response = fetch()
 
 fetch(URL)
   .then(resp => resp.json())
   .then(json => {
-    console.log(json);
+    console.log(json.data.results);
   });
 
 // 
