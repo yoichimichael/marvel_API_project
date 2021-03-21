35edd335ebb53a2edb27c05a7cd5489d17873cbd
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-03-20 21:04:03 -0400

    add md5 package and url variables

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
+const md5 = require('md5');
+
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ts = Date.now();
+const hash = md5(`${ts}${PRIVATE_KEY}${PUBLIC_KEY}`)
+const URL = `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
 
 
-console.log(process.env.PUBLIC_KEY);
+console.log({PUBLIC_KEY, PRIVATE_KEY, ts, hash});
 
 // 
